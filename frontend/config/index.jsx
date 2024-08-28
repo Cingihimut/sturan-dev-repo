59ce045f72c83206052a0bd4e63c4353e94f13a5
cingihimut <cingihimutbanget@gmail.com>
2024-08-28 19:16:33 +0700

    use wallet connect to connector

---
+++ frontend/config/index.jsx
@@ -0,0 +1,32 @@
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { cookieStorage, createStorage } from 'wagmi'
+import { baseSepolia } from 'wagmi/chains'
+
+// Your WalletConnect Cloud project ID
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+// Create a metadata object
+const metadata = {
+  name: 'sturan network',
+  description: 'Blockchain Crowdfunding',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [baseSepolia]
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  auth: {
+    email: true,
+    socials: ['google', 'x', 'github', 'discord', 'apple', 'facebook', 'farcaster'],
+  },
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+//   ...wagmiOptions // Optional - Override createConfig parameters
+})
--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -2,6 +2,10 @@ import { Inter } from "next/font/google";
 import "./globals.css"
 import Navbar from "../components/NavBar";
 import Footer from "@/components/Footer";
+import { cookieStorage, cookieToInitialState } from "wagmi";
+import { config } from "../../config";
+import Web3ModalProvider from "../../context";
+import { headers } from "next/headers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,11 +15,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+  const initialState = cookieToInitialState(config, headers().get("cookie"));
   return (
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
+        <Web3ModalProvider initialState={initialState}>
         {children}
+        </Web3ModalProvider>
         <Footer />
       </body>
     </html>
